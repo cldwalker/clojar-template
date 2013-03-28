157d8193897aaa90ec0348828c2d6ee6c6b48f45
Gabriel Horner <gabriel.horner@gmail.com>
2013-03-28 08:47:03 -0400

    only lein2 and beyond

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://github.com/cldwalker/clojar-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/lfoegal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :eval-in-leiningen true)
